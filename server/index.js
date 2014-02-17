05a1e85282bf1d38d66ba5daa0d661193f6aba0f
Gio d'Amelio <giodamelio@gmail.com>
2014-02-16 19:44:38 -0800

    Setup browserify to bundle and serve our js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var path = require("path");
 
 var express = require("express");
+var browserify = require("browserify-middleware");
 
 // Make our experess instance
 var app = express();
@@ -14,9 +15,13 @@ app.use(express.errorHandler());
 app.set("views", path.normalize(__dirname + "/../client"));
 app.set("view engine", "jade");
 
+// The root of our app
 app.get("/", function(req, res){
     res.render("index");
 });
+
+// Serve our browserify-ed javascript
+app.get("/bundle.js", browserify(path.normalize(__dirname + "/../client/main.js")));
 
 app.listen(3141);
 console.log("Listening on port 3141");
